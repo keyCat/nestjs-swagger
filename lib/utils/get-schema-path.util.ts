8626c43dff5744901e6c256b828fe8abf99bbfb1
Ilya G <igorbachevsky@gmail.com>
2023-08-04 11:20:51 +0300

    add support for `@ApiModel()` to `refs()` helper

--- lib/utils/get-schema-path.util.ts
+++ lib/utils/get-schema-path.util.ts
@@ -1,4 +1,5 @@
-import { isString } from '@nestjs/common/utils/shared.utils';
+import { isString } from "@nestjs/common/utils/shared.utils";
+import { DECORATORS } from "../constants";
 
 export function getSchemaPath(model: string | Function): string {
   const modelName = isString(model) ? model : model && model.name;
@@ -6,7 +7,10 @@ export function getSchemaPath(model: string | Function): string {
 }
 
 export function refs(...models: Function[]) {
-  return models.map((item) => ({
-    $ref: getSchemaPath(item.name)
-  }));
+  return models.map((item) => {
+    const metadata = Reflect.getMetadata(DECORATORS.API_MODEL, item)?.[0];
+    return {
+      $ref: getSchemaPath(metadata?.name ?? item.name)
+    };
+  });
 }
